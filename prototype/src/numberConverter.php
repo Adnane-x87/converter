a18842bac0e3e9ce7ebcc84de1f9b40e5ac49099
Adnane-x87 <tangerinoh40@gmail.com>
2025-09-23 16:47:12 +0100

    first commit

---
+++ prototype/src/numberConverter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+class numberConverter implements converterInterface{
+
+use FormatterTrait;
+
+private $number;
+
+
+  public function __construct(int $number){
+      $this->number = $number;
+  }
+  public function toDecimal():int{
+    return $this->number;
+  }
+
+  public function toBinary():string{
+    return decbin($this->number);
+  }
+  public function toHexa():string{
+    return strtoupper(dechex($this->number));
+  }
+  public function BitwiseAnd(int $other):int{
+    return $this->number & $other;
+  }
+    public function BitwiseOr(int $other):int{
+        return $this->number | $other;
+  }
+  public function BitwiseXor(int $other):int{
+    return $this->number ^ $other;
+  }
+  public function BitwiseNot(int $other):int{
+      return ~$this->number;
+  }
+  public function Shiftleft(int $position):int{
+    return $this->number << $position;
+  }
+  public function ShiftRight(int $position):int{
+    return $this->number >> $position;
+  }
+
+}
+
